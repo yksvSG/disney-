3fc665207b1623be1593e6e52a5b1204d9144270
yksvSG <sg.yksv77@gmail.com>
2023-03-20 23:36:59 +0900

    Row component useEffect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Container, GlobalStyle } from "./App.styled.js";
 import Banner from "./components/Banner.js";
 import Nav from "./components/Nav";
 import Category from "./components/Category";
+import Row from "./components/Row";
+import requests from "./api/requests";
 function App() {
   return (
     <>
@@ -10,6 +12,18 @@ function App() {
         <Nav />
         <Banner />
         <Category />
+        <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+        <Row
+          title="Action Movies"
+          id="AM"
+          fetchUrl={requests.fetchActionMovies}
+        />
+        <Row
+          title="Comedy Movies"
+          id="CM"
+          fetchUrl={requests.fetchComedyMovies}
+        />
       </Container>
     </>
   );
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { axiosInstance } from "../api/axios";
+import requests from "../api/requests";
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
 
-const Row = () => {
+  const fecthMovieData = useCallback(async () => {
+    const axiosReq = await axiosInstance.get(fetchUrl);
+    console.log("axiosReq", axiosReq);
+    setMovies(axiosReq.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fecthMovieData();
+  }, []);
+
   return <div>Row</div>;
 };
 
