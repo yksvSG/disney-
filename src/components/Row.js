cd05cc3d96cb80fb36e86f99d7e4b752096d31fb
yksvSG <sg.yksv77@gmail.com>
2023-03-30 14:51:08 +0900

    Swiper slide

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { axiosInstance } from "../api/axios";
 import MovieModal from "./MovieModal/MovieModal";
-import { RowPosters, Slider } from "./Row.styled";
+import { Container, Content, Wrap } from "./Row.styled";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper style
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
@@ -26,44 +35,54 @@ const Row = ({ title, id, fetchUrl }) => {
   };
   console.log("movieSelected", movieSelected);
   return (
-    <Slider>
+    <Container>
       <h2>{title}</h2>
-      <div className="slider__arrow-left">
-        <span
-          className="arrow"
-          onClick={() =>
-            (document.getElementById(id).scrollLeft -= window.innerWidth - 80)
-          }
-        >
-          {"<"}
-        </span>
-      </div>
-      <RowPosters id={id}>
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className="row__poster"
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt={movie.name}
-            onClick={() => ModalhadleClick(movie)}
-          />
-        ))}
-      </RowPosters>
-      <div className="slider__arrow-right">
-        <span
-          className="arrow"
-          onClick={() =>
-            (document.getElementById(id).scrollLeft += window.innerWidth + 80)
-          }
-        >
-          {">"}
-        </span>
-      </div>
-
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // loop 기능 사용 유무
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} // 페이지 버튼 노출 유무
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        {/* 영화 나열 구간 */}
+        <Content id={id}>
+          {movies.map((movie) => (
+            <SwiperSlide>
+              <Wrap>
+                <img
+                  key={movie.id}
+                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  alt={movie.name}
+                  onClick={() => ModalhadleClick(movie)}
+                />
+              </Wrap>
+            </SwiperSlide>
+          ))}
+        </Content>
+      </Swiper>
+      {/* 모달 구간 */}
       {modalOpen && (
         <MovieModal setModalOpen={setModalOpen} {...movieSelected} />
       )}
-    </Slider>
+    </Container>
   );
 };
 
--- src/components/Row.styled.js
+++ src/components/Row.styled.js
@@ -1,90 +1,152 @@
 import styled from "styled-components";
+import { Pagination } from "swiper";
 
-export const Slider = styled.div`
-  position: relative;
+// export const Slider = styled.div`
+//   position: relative;
 
-  h2 {
-    height: 33px;
-  }
-  .slider__arrow-left,
-  .slider__arrow-right {
-    background-clip: content-box;
-    padding: 20px 0;
-    box-sizing: border-box;
-    transform: 400ms all ease-in-out;
-    cursor: pointer;
-    width: 80px;
-    z-index: 1000;
-    position: absolute;
-    top: 0;
-    height: calc(100% + 33px);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    visibility: hidden;
+//   h2 {
+//     height: 33px;
+//   }
+//   .slider__arrow-left,
+//   .slider__arrow-right {
+//     background-clip: content-box;
+//     padding: 20px 0;
+//     box-sizing: border-box;
+//     transform: 400ms all ease-in-out;
+//     cursor: pointer;
+//     width: 80px;
+//     z-index: 1000;
+//     position: absolute;
+//     top: 0;
+//     height: calc(100% + 33px);
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     visibility: hidden;
+//   }
+
+//   .slider__arrow-left {
+//     left: 0;
+//   }
+//   .slider__arrow-right {
+//     right: 0;
+//   }
+
+//   .arrow {
+//     transition: 400ms all ease-in-out;
+//   }
+
+//   .arrow:hover {
+//     transition: 400ms all ease-in-out;
+//     transform: scale(1.5);
+//   }
+
+//   &:hover .slider__arrow-left,
+//   &:hover .slider__arrow-right {
+//     transition: 400ms all ease-in-out;
+//     visibility: visible;
+//   }
+
+//   .slider__arrow-left:hover,
+//   .slider__arrow-right:hover {
+//     background-image: rgba(20, 20, 20, 0.5);
+//     transition: 400ms all ease-in-out;
+//   }
+// `;
+
+// export const RowPosters = styled.div`
+//   display: flex;
+//   overflow-y: hidden;
+//   /* overflow-x: scroll; */
+//   &::-webkit-scrollbar {
+//     display: none;
+//   }
+//   padding: 20px 0 20px 20px;
+//   scroll-behavior: smooth;
+
+//   .row__poster {
+//     object-fit: contain; /* 오브젝트 비율 유지, 정의된 공간의 최대 확대 */
+//     width: 100%;
+//     max-height: 144px;
+//     margin-right: 10px; /* poster 우측으로 10px 짤림 해결 */
+//     transform: transform 450ms;
+//     border-radius: 4px;
+//   }
+
+//   .row__poster:hover {
+//     transform: scale(1.08);
+//   }
+
+//   @media (min-width: 1200px) {
+//     .row__poster {
+//       max-height: 160px;
+//     }
+//   }
+
+//   @media (max-width: 768px) {
+//     .row__poster {
+//       max-height: 280px;
+//     }
+//   }
+// `;
+
+export const Container = styled.div`
+  padding: 0 0 26px;
+  .swiper-pagination {
+    text-align: right !important;
   }
 
-  .slider__arrow-left {
-    left: 0;
-  }
-  .slider__arrow-right {
-    right: 0;
+  .swiper-pagination-bullet {
+    background: gray !important;
+    opacity: 1 !important;
   }
 
-  .arrow {
-    transition: 400ms all ease-in-out;
+  .swiper-pagination-bullet-active {
+    background-color: white !important;
   }
-
-  .arrow:hover {
-    transition: 400ms all ease-in-out;
-    transform: scale(1.5);
+  .swiper-button-prev {
+    color: white !important;
   }
-
-  &:hover .slider__arrow-left,
-  &:hover .slider__arrow-right {
-    transition: 400ms all ease-in-out;
-    visibility: visible;
+  .swiper-button-next {
+    color: white !important;
   }
-
-  .slider__arrow-left:hover,
-  .slider__arrow-right:hover {
-    background-image: rgba(20, 20, 20, 0.5);
-    transition: 400ms all ease-in-out;
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 1.3rem !important;
+    font-weight: 600 !important;
   }
 `;
 
-export const RowPosters = styled.div`
-  display: flex;
-  overflow-y: hidden;
-  /* overflow-x: scroll; */
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  padding: 20px 0 20px 20px;
-  scroll-behavior: smooth;
+export const Content = styled.div``;
 
-  .row__poster {
-    object-fit: contain; /* 오브젝트 비율 유지, 정의된 공간의 최대 확대 */
+export const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+    rgb(0 0 0/ 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    object-fit: cover;
+    height: 100%;
+    opacity: 1;
+    position: absolute;
     width: 100%;
-    max-height: 144px;
-    margin-right: 10px; /* poster 우측으로 10px 짤림 해결 */
-    transform: transform 450ms;
-    border-radius: 4px;
+    transition: opacity 500ms ease-in-out;
+    z-index: 1;
   }
 
-  .row__poster:hover {
-    transform: scale(1.08);
-  }
-
-  @media (min-width: 1200px) {
-    .row__poster {
-      max-height: 160px;
-    }
-  }
-
-  @media (max-width: 768px) {
-    .row__poster {
-      max-height: 280px;
-    }
+  &:hover {
+    box-shadow: rgb(0 0 0/80%) 0px 40px 58px -16px rgb(0 0 0 / 72%) 0px 30px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
   }
 `;
